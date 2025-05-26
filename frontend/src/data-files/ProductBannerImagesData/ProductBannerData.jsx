630e1cba85a354016c0bc5f0669be1c9eaabc198
safwan-ms <safwan.anasms@gmail.com>
2025-05-26 20:44:03 +0530

    fix: resolve undefined file path issue

--- frontend/src/data-files/ProductBannerImagesData/ProductBannerData.jsx
+++ frontend/src/data-files/ProductBannerImagesData/ProductBannerData.jsx
@@ -1,7 +1,7 @@
 import img1 from "./BgImages/img5.jpg";
-import img2 from "./BgImages/img2.WEBP";
-import img3 from "./BgImages/img3.WEBP";
-import img4 from "./BgImages/img4.WEBP";
+import img2 from "./BgImages/img2.webp";
+import img3 from "./BgImages/img3.webp";
+import img4 from "./BgImages/img4.webp";
 import iphone15 from "./ProductImage/iPhone-15-Pro-Max.png";
 import googlePixel from "./ProductImage/GOOGLE-PIXEL-7.png";
 import oneplus11 from "./ProductImage/oneplus11.png";
@@ -12,30 +12,31 @@ const ProductBannerData = [
     name: "iPhone 15 Pro Max",
     image: img1,
     details: "This is a description of Product 1.",
-    description: "The latest iPhone with advanced features and stunning design.",
-    productImg: iphone15
+    description:
+      "The latest iPhone with advanced features and stunning design.",
+    productImg: iphone15,
   },
   {
     name: "Samsung Galaxy S23 Ultra",
     image: img2,
     details: "This is a description of Product 2.",
     description: "Experience the power of the latest Galaxy smartphone.",
-    productImg: s23u
+    productImg: s23u,
   },
   {
     name: "Google Pixel 7",
     image: img3,
     details: "This is a description of Product 3.",
     description: "Capture stunning photos with the best camera phone.",
-    productImg: googlePixel
+    productImg: googlePixel,
   },
   {
     name: "OnePlus 11",
     image: img4,
     details: "This is a description of Product 4.",
     description: "Fast performance and sleek design in one package.",
-    productImg: oneplus11
-  }
+    productImg: oneplus11,
+  },
 ];
 
 export default ProductBannerData;
